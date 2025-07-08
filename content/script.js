797777a528b287f408dd560cb94c9e1db06f7a1e
heet-1011 <heetp1011@gmail.com>
2025-07-08 14:00:25 +0530

    deleted unecessary files|uploaded description page

--- content/script.js
+++ content/script.js
@@ -1,8 +1,34 @@
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('.header');
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
   if (window.scrollY > 50) {
-      header.classList.add('scrolled');
+    header.classList.add("scrolled");
   } else {
-      header.classList.remove('scrolled');
+    header.classList.remove("scrolled");
+  }
+});
+
+function openModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    const openModal = document.querySelector('.modal[style*="display: block"]');
+    if (openModal) {
+      closeModal(openModal.id);
+    }
   }
 });
